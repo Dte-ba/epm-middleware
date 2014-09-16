bb7fc2e79d0efeed7559d53e84688cbc050ff2e7
Delmo <delmosaurio@gmail.com>
2014-09-16 20:05:57 -0300

    ready to test

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Epm = require('epm');
 var express = require('express');
 var fs = require('fs');
@@ -7,7 +9,8 @@ module.exports = function(ops) {
   var server;
 
   ops = ops || {};
-  engines = ops.engines || [];
+  var engines = ops.engines || [];
+  var defaultRepo = ops.default || '';
 
   if (!ops.path){
     throw new Error('The repository path is not defined `path` ');
@@ -26,7 +29,7 @@ module.exports = function(ops) {
     }
 
     ops = ops || {}
-    port = ops.port || 3220;
+    var port = ops.port || 3220;
 
     server
       .on('error', function(err){
@@ -67,7 +70,43 @@ module.exports = function(ops) {
       
       res.json(data);
     });
+  });
 
+  app.get('/package/:reponame?', function(req, res, next){
+    var reponame = req.params.reponame;
+
+    if (server.listening === undefined){
+      return writeError({ error: 'The repository server is down' });
+    }
+
+    if (server.repos.length === 0){
+      return writeError({ error: 'The server not have repositories' });
+    }
+
+    if (!reponame && defaultRepo !== undefined) {
+      reponame = defaultRepo;
+    } else {
+      reponame = Object.keys(server.repos)[0];
+    }
+
+    var r = server.repos[reponame];
+
+    if (r === undefined){
+      return writeError({ error: 'The repository ' + reponame + ' doesn\'t exists' });
+    }
+
+    var resolver = r.createResolver();
+
+    resolver
+      .on('error', function(err){
+        next(err);
+      })
+      .on('complete', function(result){
+        if (!writeResolved(result, res)){
+          next(new Error('Unknown request'));
+        }
+      })
+      .request(req);
 
   });
 
@@ -76,4 +115,107 @@ module.exports = function(ops) {
   function writeError(error, res){
     return res.json(error);
   }
+
+  /**
+   * Write epm-pad-engine resutls
+   */
+  function writeResolved(info, res, statusCode){
+    statusCode = statusCode || 200;
+
+    if (info.type === 'json'){
+
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(JSON.stringify(info.data));
+
+      return true;
+    } else if (info.type === 'file') {
+      // content || assets
+      res.sendFile(info.filename);
+
+      return true;
+    } else if (info.type === 'files') {
+      // content with mutiple files
+      if (info.files.length === 1){
+        res.sendFile(info.files[0].filename);
+      } else {
+        var bpath = extractBasepath(info.files.map(function(f){ return f.filename; }));
+
+        var relfiles = info.files.map(function(f){
+          var fname = f.filename.replace(/\\/ig, '/');
+          return fname.replace(bpath, '');
+        });
+
+        var htmls = relfiles.filter(function(rf){
+          return (/\.html$/i).test(rf);
+        });
+
+        // the index HTML
+        var ihtml = htmls.filter(function(h){ 
+          return /index.htm(l)?/ig.test(h);
+        });
+
+        var idxhtml = ihtml.length > 0 ? ihtml[0] : undefined;
+
+        if ( idxhtml === undefined){
+          // index.html doesn't exists 
+          // any html
+          idxhtml = (htmls.length > 0 ? htmls[0] : undefined)
+        }
+
+        var route = '/content/' + info.uid;
+        if (app.cacheContent[route] === undefined){
+          app.cacheContent[route] = {
+            path: bpath,
+            index: idxhtml
+          };
+        }
+        var r = route;
+        var item = app.cacheContent[route];
+        if (item.index !== undefined){
+          r += '/' + item.index;
+        }
+        res.redirect(r);
+      }
+
+      return true;
+    }
+
+    return false;
+
+    function extractBasepath(entries) {
+
+      var dirs = entries.map(function(e){ return path.dirname(e); });
+
+      var sdirs = dirs.map(function(d){ return d.split('\\')});
+
+      var idx = 0;
+      var curr;
+      var root = '';
+      var eq;
+
+      do {
+
+        curr = sdirs[0][idx];
+
+        var have = _.all(sdirs, function(sd){ return sd.length > idx; });
+        eq = have;
+        if (have === true){
+          eq = _.all(sdirs.map(function(a){ return a[idx]; }), function(i){
+            return curr === i;
+          });
+        }
+
+        if(eq) {
+          root += curr + '/';
+        }
+
+        idx++;
+
+      } while(eq === true)
+
+      return root;
+    }
+  }
+
 };
